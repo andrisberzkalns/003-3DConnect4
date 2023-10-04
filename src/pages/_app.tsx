9a844a2bb7727b89d512e791586ae6e6afa02afa
andrisberzkalns <berzkalns.andris@gmail.com>
2023-10-04 22:37:34 +0300

    Google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 
 import { api } from "~/utils/api";
@@ -21,6 +22,22 @@ const MyApp: AppType<{ session: Session | null }> = ({
           content="Play 3D Connect 4 game online for free. No download required."
         />
         <link rel="icon" href="/favicon.png" />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-1D3S5SCDB0`}
+        />
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-1D3S5SCDB0');
+            `,
+          }}
+        />
       </Head>
       <Toaster position="top-center" />
       <Component {...pageProps} />
